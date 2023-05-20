94dfd4408c1d2cb1c30077cfae042f03d372aa85
Pedro Lima <pedro@deonibus.com>
2023-05-20 17:14:38 -0300

    Fix git ignore bugs

---
+++ Infra.Data/CarlletDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Data
+{
+    public class CarlletDbContext : DbContext
+    {
+        public CarlletDbContext (DbContextOptions<CarlletDbContext> options): base(options)
+        {}
+    }
+}
