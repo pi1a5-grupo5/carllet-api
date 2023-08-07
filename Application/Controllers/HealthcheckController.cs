de328c36e167c56c11e25c600492b3533d855e2e
Pedro Lima <pedro@deonibus.com>
2023-08-07 18:51:48 -0300

    feat: adicionado dockerfile e healthcheck

---
+++ Application/Controllers/HealthcheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthcheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("OK");
+        }
+    }
+}
